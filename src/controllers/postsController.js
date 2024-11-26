55542d951a5d43da8cdd500aecf3b8351137c720
Cleiton dos Santos Oliveira <cleiton.santos@ana.gov.br>
2024-11-26 01:30:36 -0300

    commit imersao backend

---
+++ src/controllers/postsController.js
@@ -0,0 +1,59 @@
+import { atualizarPost, criarPost, getTodosPosts } from "../models/postsModels.js";
+import fs from "fs";
+import gerarDescricaoComGemini from "../services/geminiService.js";
+
+
+export async function listarPosts (req, res)
+{
+    const posts =  await getTodosPosts();
+    res.status(200).json(posts);
+}
+
+export async function postarNovoPost(req,res) {
+    const novoPost = req.body;
+    try{
+        const postCriado = await criarPost(novoPost);
+        res.status(200).json(postCriado);
+    } catch (erro){
+        console.error(erro.message);
+        res.status(500).json({"Erro":"falha na resquisição"})
+    }
+}
+
+export async function uploadImagem(req, res) {
+    const novoPost = {
+        descricao: "foto teste",
+        imagem: req.file.originalname,
+        alt:""
+    };
+    try{
+        const postCriado = await criarPost(novoPost);
+        const imagemAtualizada = `uploads/${postCriado.insertedId}.png`;
+        fs.renameSync(req.file.path, imagemAtualizada);
+        res.status(200).json(postCriado);
+    } catch (erro){
+        console.error(erro.message);
+        res.status(500).json({"Erro":"falha na resquisição"})
+    }
+}
+
+export async function atualizarNovoPost(req, res) {
+    const id = req.params.id;
+    const urlImagem = `http://localhost:3000/${id}.png`
+    try{
+        const imgBuffer = fs.readFileSync(`uploads/${id}.png`)
+        const descricao = await gerarDescricaoComGemini(imgBuffer);
+        const post = {
+            imgUrl: urlImagem,
+            descricao: descricao,
+            alt: req.body.alt
+        }
+    
+        const postCriado = await atualizarPost(id, post);
+        const imagemAtualizada = `uploads/${postCriado.insertedId}.png`;
+        res.status(200).json(postCriado);
+    } catch (erro){
+        console.error(erro.message);
+        res.status(500).json({"Erro":"falha na resquisição"})
+    }
+}
